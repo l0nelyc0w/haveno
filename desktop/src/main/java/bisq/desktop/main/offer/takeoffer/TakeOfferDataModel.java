da7d97084904ca1cafea5bf17ffd2d05b3ba0179
premek <1145361+premek@users.noreply.github.com>
2021-07-11 23:20:29 +0200

    remove commented out code

--- desktop/src/main/java/bisq/desktop/main/offer/takeoffer/TakeOfferDataModel.java
+++ desktop/src/main/java/bisq/desktop/main/offer/takeoffer/TakeOfferDataModel.java
@@ -645,17 +645,6 @@ class TakeOfferDataModel extends OfferDataModel {
     public Coin getSellerSecurityDeposit() {
         return offer.getSellerSecurityDeposit();
     }
-    /*
-    public Coin getUsableBsqBalance() {
-        // we have to keep a minimum amount of BSQ == bitcoin dust limit
-        // otherwise there would be dust violations for change UTXOs
-        // essentially means the minimum usable balance of BSQ is 5.46
-        Coin usableBsqBalance = bsqWalletService.getAvailableConfirmedBalance().subtract(Restrictions.getMinNonDustOutput());
-        if (usableBsqBalance.isNegative())
-            usableBsqBalance = Coin.ZERO;
-        return usableBsqBalance;
-    }
-    */
 
     public boolean isHalCashAccount() {
         return paymentAccount.isHalCashAccount();
